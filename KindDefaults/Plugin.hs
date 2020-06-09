dd3035f08f3d44bd84e840304a47f65cfc0aea91
Matthías Páll Gissurarson <mpg@mpg.is>
2020-06-09 17:08:28 +0200

    Works!

--- KindDefaults/Plugin.hs
+++ KindDefaults/Plugin.hs
@@ -5,7 +5,7 @@ module KindDefaults.Plugin  (plugin) where
 import GhcPlugins hiding (TcPlugin)
 import TcRnTypes
 import TcPluginM
-import Control.Monad (when, guard)
+import Control.Monad (when, join)
 import Constraint
 import Data.Maybe (fromJust, isJust, mapMaybe)
 import Data.IORef
@@ -21,6 +21,7 @@ import FamInstEnv
 import Finder 
 
 import TysPrim (eqPrimTyCon)
+import Predicate
 
 
 import GHC.Hs
@@ -57,8 +58,8 @@ addWarning dflags log = warn (logMsg log)
     warn =
       putLogMsg dflags NoReason SevWarning (logSrc log) (defaultErrStyle dflags)
 
-logDefaulting :: DynFlags -> (TyCoVar, Kind, Type, SrcSpan) -> Log
-logDefaulting _ (var, kind, ty, loc) = Defaulting var kind ty loc
+logDefaulting :: DynFlags -> (TyCoVar, Kind, Type, CtLoc) -> Log
+logDefaulting _ (var, kind, ty, loc) = Defaulting var kind ty (RealSrcSpan $ ctLocSpan loc)
 
 logMsg :: Log -> SDoc
 logMsg (Defaulting var kind ty _) = text "Defaulting" <+> ppr (occName var) <+>
@@ -83,20 +84,38 @@ kindDefaultPlugin opts = TcPlugin initialize solve stop
         ; defaultToTyCon <- getDefaultTyCon
         ; let tvs = map (getTyVarDefaults instEnvs defaultToTyCon) wanted
         ; tcPluginIO $ modifyIORef warns (concatMap ( map (logDefaulting dflags)) tvs ++)
-        ; mapM_ (pprDebug "TyVars:" ) tvs
+        ; mapM_ (pprDebug "TyVars:" . (map (\(a,b,c,_) -> (a,b,c)))) tvs
         ; let defs =  map (map (defaultEqConstraint defaultToTyCon)) tvs
         ; mapM_ (pprDebug "Defs:" ) defs
-        ; return $ TcPluginOk [] [] }
+        ; defCts <- concat <$> mapM (mapM (defaultingCt defaultToTyCon)) tvs
+        ; mapM_ (pprDebug "Defs:" ) defCts
+        ; return $ TcPluginOk [] defCts }
      stop warns =
         do { dflags <- unsafeTcPluginTcM getDynFlags
            ; tcPluginIO $ readIORef warns >>=
                           mapM_ (addWarning dflags) . sort . nub }
 
-defaultEqConstraint :: TyCon -> (TyCoVar, Kind, Type, SrcSpan) -> Type
-defaultEqConstraint defaultTyCon (ty, kind, var, _) = 
-      mkTyConApp eqPrimTyCon [kind, kind, mkTyVarTy ty, eqTo]
+defaultEqConstraint :: TyCon -> (TyCoVar, Kind, Type, CtLoc) -> Type
+defaultEqConstraint defaultTyCon (var, kind, def, _) = 
+      mkTyConApp eqPrimTyCon [kind, kind, mkTyVarTy var, eqTo]
  where --eqTo = var -- if we want direct
        eqTo = mkTyConApp defaultTyCon [kind]
+
+defaultingCt :: TyCon -> (TyCoVar, Kind, Type, CtLoc) -> TcPluginM Ct
+defaultingCt defaultTyCon (var, kind, def, loc) = 
+   do ev <- getEv
+      return $ CTyEqCan {cc_ev = ev, cc_tyvar = var,
+                         cc_rhs = eqTo, cc_eq_rel=eqRel }
+ where --eqTo = var -- if we want direct
+       eqTo = mkTyConApp defaultTyCon [kind]
+       predTy = mkTyConApp eqPrimTyCon [kind, kind, mkTyVarTy var, eqTo]
+       eqRel = NomEq
+       getEv = do ref <- tcPluginIO $ newIORef Nothing
+                  let hole = CoercionHole {ch_co_var = var, ch_ref=ref} 
+                  return $ CtWanted {ctev_pred = predTy, 
+                                     ctev_nosh = WDeriv,
+                                     ctev_dest = HoleDest hole,
+                                     ctev_loc = loc}
 
 getDefaultTyCon :: TcPluginM TyCon
 getDefaultTyCon =
@@ -108,14 +127,14 @@ getDefaultTyCon =
                tcLookupTyCon name
          _ -> pprPanic "DefaultTo not found!" empty
 
-getTyVarDefaults :: FamInstEnvs -> TyCon -> Ct -> [(TyCoVar, Kind, Type, SrcSpan)]
+getTyVarDefaults :: FamInstEnvs -> TyCon -> Ct -> [(TyCoVar, Kind, Type, CtLoc)]
 getTyVarDefaults famInsts defaultToTyCon ct = mapMaybe getDefault tvs
   where tvs = tyCoVarsOfCtList ct 
         lookup kind = lookupFamInstEnv famInsts defaultToTyCon [kind]
         getDefault ty =
            case lookup  (varType ty) of
               [FamInstMatch {fim_instance=FamInst{fi_rhs=def}}] ->
-                 Just (ty, varType ty, def, RealSrcSpan $ ctLocSpan $ ctLoc ct)
+                 Just (ty, varType ty, def, ctLoc ct)
               _ -> Nothing
 
 isPrimEqTyCon :: TyCon -> Bool
--- Test/Test.hs
+++ Test/Test.hs
@@ -12,22 +12,20 @@
 {-# LANGUAGE TypeApplications #-}
 module Main where
 
-import KindDefaults (DefaultTo(..))
+import KindDefaults (DefaultTo)
 
 
 data Label = L
            | H 
            deriving (Show)
 
-type instance DefaultTo Label = L
-
+type instance DefaultTo Label = H
 
 data F (a :: Label) = F deriving (Show)
 
 class Less (l :: Label) (l' :: Label) where
 instance Less L H where
 instance Less l l where
-
 
 f :: (Less a H) => F a -> F H
 f F = F
